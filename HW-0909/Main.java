6cecaaadc866647b386859fadf814b0650b64380
Tetiana Koval <t.koval@u-commodities.com>
2022-09-14 23:13:04 +0300

    Home work Tasks 09.09 цикл for вывод чисел+сумма выведенных чисел

---
+++ HW-0909/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Татьяна Коваль, группа 35-36ma
+        // First level: C помощью цикла for написать метод, печатающий цифры от 1 до  числа  n
+
+             int n=20;
+        for (int i=1; i<=n; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ HW-0909/Main2.java
@@ -0,0 +1,16 @@
+public class Main2 {
+    public static void main(String[] args) {
+
+        //Татьяна Коваль, группа 35-36ma
+        // Second level: C помощью цикла for написать метод возвращающий сумму цифр от 1 до числа n
+
+        int n = 10;
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+
+            sum = sum + i;
+           }
+        System.out.println(sum);
+        }
+    }
+
